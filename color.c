411aab2131c8086fb9d13e5ca7dd780863afe0c9
Thomas E. Dickey <dickey@invisible-island.net>
1996-06-25 23:15:22 -0400

    snapshot of project "vttest", label v2_0

---
+++ color.c
@@ -0,0 +1,485 @@
+/* $Id: color.c,v 1.7 1996/06/25 10:12:17 tom Exp $ */
+
+#include <vttest.h>
+#include <esc.h>
+#include <color.h>
+
+#define MAX_COLORS    8
+
+#define COLOR_BLACK   0
+#define COLOR_RED     1
+#define COLOR_GREEN   2
+#define COLOR_YELLOW  3
+#define COLOR_BLUE    4
+#define COLOR_MAGENTA 5
+#define COLOR_CYAN    6
+#define COLOR_WHITE   7
+
+static char     *colors[MAX_COLORS] =
+{
+  "black",       /* 30, 40 */
+  "red",         /* 31, 41 */
+  "green",       /* 32, 42 */
+  "yellow",      /* 33, 43 */
+  "blue",        /* 34, 44 */
+  "magenta",     /* 35, 45 */
+  "cyan",        /* 36, 46 */
+  "white"        /* 37, 47 */
+};
+
+static int next_word(char *s);
+static void c_sgr(char *s);
+static void draw_box_caption(int x0, int y0, int x1, int y1, char **c);
+static void draw_box_outline(int x0, int y0, int x1, int y1, int c);
+static void draw_hline(int x0, int y0, int x1, int c);
+static void draw_vline(int x0, int y0, int y1, int c);
+static void reset_colors(void);
+static void set_background(int bg);
+static void set_color_pair(int fg, int bg);
+static void set_foreground(int fg);
+static void show_graphic_rendition(void);
+static void show_line_deletions(void);
+static void show_line_insertions(void);
+static void show_test_pattern(void);
+static void simple_bce_test(void);
+static void test_color_insdel(void);
+
+/*
+ * Pick an unusual color combination for testing, just in case the user's
+ * got the background set to something different.
+ */
+static void
+c_sgr(char *s)
+{
+  char temp[80];
+  char *t = strchr(strcpy(temp, s), '0');
+  int reset = FALSE;
+
+  if (*temp == ';' || *temp == 0) {
+    reset = TRUE;
+  } else {
+    for (t = temp; *t != 0; t++) {
+      if (((t[0] == '0')
+        && (t == temp || t[-1] == ';')
+	&& (t[1] == 0 || t[1] == ';'))
+       || ((t[0] == ';')
+        && (t[1] == ';'))) {
+	reset = TRUE;
+	break;
+      }
+    }
+  }
+
+  if (reset) {
+    sprintf(temp + strlen(temp), ";%d;%d", COLOR_YELLOW + 30, COLOR_GREEN + 40);
+  }
+  sgr(temp);
+}
+
+static void
+draw_box_caption(int x0, int y0, int x1, int y1, char ** c)
+{
+  int x = x0, y = y0;
+  int t;
+  char *s;
+
+  while ((s = *c++) != 0) {
+    while ((t = *s++) != 0) {
+      if (x == x0) {
+        if (t == ' ')
+          continue;
+        cup(y, x);
+        putchar(' '); x++;
+      }
+      putchar(t); x++;
+      if ((t == ' ') && (next_word(s) > (x1-x-2))) {
+	while (x < x1) {
+	  putchar(' '); x++;
+	}
+      }
+      if (x >= x1) {
+        putchar(' ');
+        x = x0;
+        y++;
+      }
+    }
+  }
+  while (y <= y1) {
+    if (x == x0) {
+      cup(y, x);
+    }
+    putchar(' ');
+    if (++x >= x1) {
+      putchar(' ');
+      x = x0;
+      y++;
+    }
+  }
+}
+
+static void
+draw_box_outline(int x0, int y0, int x1, int y1, int c)
+{
+  draw_hline(x0, y0, x1, c);
+  draw_hline(x0, y1, x1, c);
+  draw_vline(x0, y0, y1, c);
+  draw_vline(x1, y0, y1, c);
+}
+
+static void
+draw_hline(int x0, int y0, int x1, int c)
+{
+  int n;
+
+  cup(y0, x0);
+  for (n = x0; n <= x1; n++)
+    putchar(c);
+}
+
+static void
+draw_vline(int x0, int y0, int y1, int c)
+{
+  int n;
+
+  for (n = y0; n <= y1; n++) {
+    cup(n, x0);
+    putchar(c);
+  }
+}
+
+static int
+next_word(char *s)
+{
+  char *base;
+  while (*s == ' ')
+    s++;
+  base = s;
+  while (*s && *s != ' ')
+    s++;
+  return (s - base);
+}
+
+/*
+ * Some terminals will reset colors with SGR-0; I've added the 39, 49 codes for
+ * those that are ISO compliant.
+ */
+static void
+reset_colors(void)
+{
+  sgr("0;39;49");
+}
+
+static void
+set_background(int bg)
+{
+  char temp[80];
+  (void)sprintf(temp, "4%d", bg);
+  sgr(temp);
+}
+
+static void
+set_color_pair(int fg, int bg)
+{
+  char temp[80];
+  (void)sprintf(temp, "3%d;4%d", fg, bg);
+  sgr(temp);
+}
+
+static void
+set_foreground(int fg)
+{
+  char temp[80];
+  (void)sprintf(temp, "3%d", fg);
+  sgr(temp);
+}
+
+static void
+set_test_colors(void)
+{
+  c_sgr("0");
+}
+
+/* Graphic rendition requires special handling with color, since SGR-0
+ * is supposed to reset the colors as well.
+ */
+static void
+show_graphic_rendition(void)
+{
+  ed(2);
+  cup( 1,20); printf("Color/Graphic rendition test pattern:");
+  cup( 4, 1); c_sgr("0");            printf("vanilla");
+  cup( 4,40); c_sgr("0;1");          printf("bold");
+  cup( 6, 6); c_sgr(";4");           printf("underline");
+  cup( 6,45); c_sgr(";1");c_sgr("4");printf("bold underline");
+  cup( 8, 1); c_sgr("0;5");          printf("blink");
+  cup( 8,40); c_sgr("0;5;1");        printf("bold blink");
+  cup(10, 6); c_sgr("0;4;5");        printf("underline blink");
+  cup(10,45); c_sgr("0;1;4;5");      printf("bold underline blink");
+  cup(12, 1); c_sgr("1;4;5;0;7");    printf("negative");
+  cup(12,40); c_sgr("0;1;7");        printf("bold negative");
+  cup(14, 6); c_sgr("0;4;7");        printf("underline negative");
+  cup(14,45); c_sgr("0;1;4;7");      printf("bold underline negative");
+  cup(16, 1); c_sgr("1;4;;5;7");     printf("blink negative");
+  cup(16,40); c_sgr("0;1;5;7");      printf("bold blink negative");
+  cup(18, 6); c_sgr("0;4;5;7");      printf("underline blink negative");
+  cup(18,45); c_sgr("0;1;4;5;7");    printf("bold underline blink negative");
+  cup(20, 6); c_sgr(""); set_foreground(9); printf("original foreground");
+  cup(20,45); c_sgr(""); set_background(9); printf("original background");
+  c_sgr(""); /* same as c_sgr("0") */
+
+  rm("?5"); /* Inverse video off */
+  cup(max_lines-1,1); el(0); printf("Dark background. "); holdit();
+
+  sm("?5"); /* Inverse video */
+  cup(max_lines-1,1); el(0); printf("Light background. "); holdit();
+  rm("?5");
+}
+
+static void
+show_line_deletions(void)
+{
+  int row;
+
+  ed(2);
+  cup(1,1);
+  printf("This test deletes every third line from a list, marking cursor with '*'.\n");
+  printf("The foreground and background should be yellow(orange) and blue, respectively.\n");
+
+  for (row = 5; row <= max_lines; row++) {
+    cup(row,1);
+    printf("   row %3d: this is some text", row);
+  }
+  for (row = 7; row <= max_lines; row += 2 /* 3 - deletion */) {
+    cup(row,1);
+    dl(1);
+    putchar('*');  /* cursor should be in column 1 */
+  }
+  cup(3,1);
+  holdit();
+}
+
+static void
+show_line_insertions(void)
+{
+  int row;
+
+  ed(2);
+  cup(1,1);
+  printf("This test inserts after every second line in a list, marking cursor with '*'.\n");
+  printf("The foreground and background should be yellow(orange) and blue, respectively.\n");
+
+  for (row = 5; row <= max_lines; row++) {
+    cup(row,1);
+    printf("   row %3d: this is some text", row);
+  }
+  for (row = 7; row <= max_lines; row += 3 /* 2 + insertion */) {
+    cup(row,1);
+    il(1);
+    putchar('*');  /* cursor should be in column 1 */
+  }
+  cup(3,1);
+  holdit();
+}
+
+static void
+show_test_pattern(void)
+/* generate a color test pattern */
+{
+  int i, j, k;
+
+  reset_colors();
+  ed(2);
+  cup(1,1);
+  printf("There are %d color combinations", MAX_COLORS * MAX_COLORS);
+
+  for (k = 0; k <= 11; k += 11) {
+    cup(k + 2, 1);
+    printf("%dx%d matrix of foreground/background colors, bright *",
+      MAX_COLORS, MAX_COLORS);
+
+    if (k) {
+      sgr("1");
+      printf("on");
+      sgr("0");
+    } else {
+      printf("off");
+    }
+    printf("*");
+
+    for (i = 0; i < MAX_COLORS; i++) {
+      cup(k + 3, (i+1) * 8 + 1);
+      printf("%s", colors[i]);
+    }
+
+    for (i = 0; i < MAX_COLORS; i++) {
+      cup(k + i + 4, 1);
+      printf("%s", colors[i]);
+    }
+
+    for (i = 0; i < MAX_COLORS; i++) {
+      for (j = 0; j < MAX_COLORS; j++) {
+        if (k)
+          sgr("1");
+        set_color_pair(j, i);
+        cup(k + 4 + i, (j+1) * 8 + 1);
+        printf("Hello");
+        reset_colors();
+      }
+    }
+  }
+  reset_colors();
+  cup(max_lines-1, 1);
+  holdit();
+}
+
+/*
+ * "Real" color terminals support bce (background color erase).
+ *
+ * Set the foreground and background colors to something that's unusual.
+ * Then clear the screen (the background should stick) and draw some nested
+ * boxes (because that's simple). Use the ED, EL controls to clear away the
+ * outer box, so we can exercise the various parameter combinations of each
+ * of these.
+ */
+static void
+simple_bce_test(void)
+{
+  int i, j;
+  static int top  = 3,    top2  = 7;    /* box margins */
+  static int left = 10,   left2 = 18;
+  static char *text1[] = {
+    "The screen background should be blue, with a box made of asterisks",
+    " and this caption, in orange (non-bold yellow). ",
+    " There should be no cells with the default foreground or background.",
+    0
+  };
+  static char *text2[] = {
+    "The screen background should be black, with a box made of asterisks",
+    " and this caption, in white (non-bold yellow). ",
+    " Only the asterisk box should be in color.",
+    0
+  };
+
+  set_test_colors();
+  ed(2);
+
+  for (i = top; i < max_lines-top; i++) {
+    cup(i, left);
+    for (j = left; j < min_cols-left; j++) {
+      putchar('X');
+    }
+  }
+
+  draw_box_outline(left2, top2, min_cols-left2, max_lines-top2, '*');
+
+  cup(top2-1, min_cols/2);
+  ed(1); /* clear from home to cursor */
+  cuf(1);
+  el(0); /* clear from cursor to end of line */
+
+  cup(max_lines - (top2-1), min_cols/2);
+  ed(0); /* clear from cursor to end */
+  cub(1);
+  el(1); /* clear to beginning of line */
+
+  for (i = top2; i <= max_lines-top2; i++) {
+    cup(i, left2-1);
+    el(1);
+    cup(i, min_cols - (left2-1));
+    el(0);
+  }
+
+  draw_box_caption(left2+1, top2+1, min_cols-left2-1, max_lines-top2-1, text1);
+
+  cup(max_lines-1, 1);
+  holdit();
+
+  /* Now, set the background again just in case there's a glitch */
+  set_foreground(COLOR_WHITE);
+  set_background(COLOR_BLACK);
+
+  cup(top2-1, min_cols/2);
+  ed(1); /* clear from home to cursor */
+  cuf(1);
+  el(0); /* clear from cursor to end of line */
+
+  cup(max_lines - (top2-1), min_cols/2);
+  ed(0); /* clear from cursor to end */
+  cub(1);
+  el(1); /* clear to beginning of line */
+
+  for (i = top2; i <= max_lines-top2; i++) {
+    cup(i, left2-1);
+    el(1);
+    cup(i, min_cols - (left2-1));
+    el(0);
+  }
+
+  draw_box_caption(left2+1, top2+1, min_cols-left2-1, max_lines-top2-1, text2);
+
+  cup(max_lines-1, 1);
+  holdit();
+
+  reset_colors();
+}
+
+/*
+ * Test the insert/delete line/character operations for color (bce) terminals
+ * We'll test insert/delete line operations specially, because it is very hard
+ * to see what is happening with the accordion test when it does not work.
+ */
+static void
+test_color_insdel(void)
+{
+  set_test_colors();
+
+  show_line_insertions();
+  show_line_deletions();
+
+  /* The rest of the test can be done nicely with the standard vt100 test
+   * for insert/delete, since it doesn't modify SGR.
+   */
+  tst_insdel();
+  reset_colors();
+}
+
+static void
+test_color_screen(void)
+{
+  set_test_colors();
+
+  do_scrolling();
+  show_graphic_rendition();
+  reset_colors();
+}
+
+/*
+ * For terminals that support ANSI/ISO colors, work through a graduated
+ * set of tests that first display colors (if the terminal does indeed
+ * support them), then exercise the associated reset, clear operations.
+ */
+void
+tst_colors(void)
+{
+  int menuchoice;
+  static char *colormenu[] = {
+    "Return to main menu",
+    "Display color test-pattern",
+    "Test BCE-style clear line/display",
+    "Test of VT102-style features with BCE (Insert/Delete Char/Line)",
+    "Test of screen features with BCE",
+    ""
+  };
+
+  do {
+    ed(2);
+    cup(7,10); println("Choose test type:");
+    menuchoice = menu(colormenu);
+    switch (menuchoice) {
+    case 1: show_test_pattern(); break;
+    case 2: simple_bce_test();   break;
+    case 3: test_color_insdel(); break;
+    case 4: test_color_screen(); break;
+    }
+  } while (menuchoice);
+}
---
+++ ttymodes.c
@@ -0,0 +1,285 @@
+/* $Id: ttymodes.c,v 1.6 1996/06/21 21:07:40 tom Exp $ */
+
+#include <vttest.h>
+#include <ttymodes.h>
+#include <esc.h>	/* inflush() */
+
+static TTY old_modes, new_modes;
+
+#if !USE_POSIX_TERMIOS && !USE_TERMIO && USE_SGTTY
+static struct tchars  old_tchars;
+static struct ltchars old_ltchars;
+#endif
+
+#if USE_POSIX_TERMIOS || USE_TERMIO
+static void
+disable_control_chars(TTY *modes)
+{
+# if USE_POSIX_TERMIOS
+    int n;
+    int temp;
+#   if defined(_POSIX_VDISABLE) && (_POSIX_VDISABLE != -1)
+      temp = _POSIX_VDISABLE;
+#   else
+      errno = 0;
+      temp = fpathconf(0, _PC_VDISABLE);
+      if (temp == -1) {
+	if (errno != 0) {
+	  restore_ttymodes();
+	  fprintf(stderr, "Cannot disable special characters!\n");
+	  exit(EXIT_FAILURE);
+	}
+	temp = 0377;
+      }
+#     endif
+    for (n = 0; n < NCCS; n++)
+      modes->c_cc[n] = temp;
+# else	/* USE_TERMIO */
+#   ifdef	VSWTCH
+      modes->c_cc[VSWTCH] = VDISABLE;
+#   endif
+    modes->c_cc[VSUSP]  = VDISABLE;
+#   if defined (VDSUSP) && defined(NCCS) && VDSUSP < NCCS
+      modes->c_cc[VDSUSP]  = VDISABLE;
+#   endif
+    modes->c_cc[VSTART] = VDISABLE;
+    modes->c_cc[VSTOP]  = VDISABLE;
+# endif
+    modes->c_cc[VMIN]  = 1;
+    modes->c_cc[VTIME] = 0;
+}
+#endif
+
+static void
+set_ttymodes(TTY *modes)
+{
+# if USE_POSIX_TERMIOS
+    fflush(stdout);
+    tcsetattr(0, TCSAFLUSH, modes);
+# else
+#   if USE_TERMIO
+      tcsetattr(0, TCSETAF, modes);
+#   else /* USE_SGTTY */
+      stty(0, modes);
+#   endif
+# endif
+}
+
+#ifndef log_ttymodes
+void log_ttymodes(char *file, int line)
+{
+  FILE *fp = fopen("ttymodes.log", "a");
+  if (fp != 0) {
+    fprintf(fp, "%s @%d\n", file, line);
+    fclose(fp);
+  }
+}
+#endif
+
+#ifndef dump_ttymodes
+void dump_ttymodes(char *tag, int flag)
+{
+#ifdef UNIX
+  FILE *fp = fopen("ttymodes.log", "a");
+  TTY tmp_modes;
+  if (fp != 0) {
+    fprintf(fp, "%s (%d):\n", tag, flag);
+# if USE_POSIX_TERMIOS || USE_TERMIO
+    tcgetattr(0, &tmp_modes);
+    fprintf(fp, " iflag %08o\n", tmp_modes.c_iflag);
+    fprintf(fp, " oflag %08o\n", tmp_modes.c_oflag);
+    fprintf(fp, " lflag %08o\n", tmp_modes.c_lflag);
+    if (!tmp_modes.c_lflag & ICANON) {
+      fprintf(fp, " %d:min  =%d\n", VMIN,  tmp_modes.c_cc[VMIN]);
+      fprintf(fp, " %d:time =%d\n", VTIME, tmp_modes.c_cc[VTIME]);
+    }
+# else
+    gtty(0, &tmp_modes);
+    fprintf(fp, " flags %08o\n", tmp_modes.sg_flags);
+# endif
+    fclose(fp);
+  }
+#endif
+}
+#endif
+
+void init_ttymodes(int pn)
+{
+  dump_ttymodes("init_ttymode", pn);
+#ifdef UNIX
+  if (pn==0) {
+    fflush(stdout);
+# if USE_POSIX_TERMIOS || USE_TERMIO
+    tcgetattr(0, &old_modes);
+# else
+#   if USE_SGTTY
+      gtty(0, &old_modes);
+      ioctl(0, TIOCGETC, &old_tchars);
+      ioctl(0, TIOCGLTC, &old_ltchars);
+#   endif
+# endif
+    new_modes = old_modes;
+  } else {
+    fflush(stdout);
+    inflush();
+    sleep(2);
+  }
+# if USE_POSIX_TERMIOS || USE_TERMIO
+    new_modes.c_iflag = BRKINT | old_modes.c_iflag;
+# else /* USE_SGTTY */
+    new_modes.sg_flags = old_modes.sg_flags | CBREAK;
+# endif
+  set_ttymodes(&new_modes);
+# if USE_FCNTL
+  close(2);
+  open("/dev/tty", O_RDWR|O_NDELAY);
+# endif
+#endif /* UNIX */
+#ifdef SARG10
+  /* Set up neccesary TOPS-10 terminal parameters	*/
+
+  trmop(02041, `VT100`);	/* tty type vt100	*/
+  trmop(02002, 0);	/* tty no tape	*/
+  trmop(02003, 0);	/* tty lc	*/
+  trmop(02005, 1);	/* tty tab	*/
+  trmop(02010, 1);	/* tty no crlf	*/
+  trmop(02020, 0);	/* tty no tape	*/
+  trmop(02021, 1);	/* tty page	*/
+  trmop(02025, 0);	/* tty blanks	*/
+  trmop(02026, 1);	/* tty no alt	*/
+  trmop(02040, 1);	/* tty defer	*/
+#endif
+#ifdef SARG20
+  ttybin(1);	/* set line to binary mode */
+#endif
+  dump_ttymodes("...init_ttymode", pn);
+}
+
+void restore_ttymodes(void)
+{
+  dump_ttymodes("restore_ttymodes", -1);
+#ifdef SARG20
+  ttybin(0);	/* reset line to normal mode */
+#endif
+#ifdef UNIX
+  set_ttymodes(&old_modes);
+#endif
+  dump_ttymodes("...restore_ttymodes", -1);
+}
+
+void
+set_tty_crmod(int enabled)
+{
+  dump_ttymodes("set_tty_crmod", enabled);
+#ifdef UNIX
+# if USE_POSIX_TERMIOS || USE_TERMIO
+#   if USE_POSIX_TERMIOS
+#     define MASK_CRMOD ((unsigned long) (INLCR | IGNCR | ICRNL | IXON))
+#   else
+#     define MASK_CRMOD ((unsigned long) (INLCR | IGNCR | ICRNL))
+#   endif
+    if (enabled) {
+      new_modes.c_iflag |= MASK_CRMOD;
+      new_modes.c_lflag |= ICANON;
+      memcpy(new_modes.c_cc, old_modes.c_cc, sizeof(new_modes.c_cc));
+    } else {
+      new_modes.c_iflag &= ~MASK_CRMOD;
+      new_modes.c_lflag &= ~ICANON;
+      disable_control_chars(&new_modes);
+    }
+# else
+    if (enabled)
+      new_modes.sg_flags |= CRMOD;
+    else
+      new_modes.sg_flags &= ~CRMOD;
+# endif
+  set_ttymodes(&new_modes);
+#endif
+  dump_ttymodes("...set_tty_crmod", enabled);
+}
+
+void
+set_tty_echo(int enabled)
+{
+  dump_ttymodes("set_tty_echo", enabled);
+#ifdef UNIX
+# if USE_POSIX_TERMIOS || USE_TERMIO
+    if (enabled)
+      new_modes.c_lflag |= ECHO;
+    else
+      new_modes.c_lflag &= ~ECHO;
+# else /* USE_SGTTY */
+    if (enabled)
+      new_modes.sg_flags |= ECHO;
+    else
+      new_modes.sg_flags &= ~ECHO;
+# endif
+  set_ttymodes(&new_modes);
+#endif
+  dump_ttymodes("...set_tty_echo", enabled);
+}
+
+void
+set_tty_raw(int enabled)
+{
+  dump_ttymodes("set_tty_raw", enabled);
+  if (enabled) {
+#ifdef UNIX
+# if USE_POSIX_TERMIOS || USE_TERMIO
+    new_modes.c_iflag      = 0;
+    new_modes.c_oflag      = 0;
+    new_modes.c_lflag      = 0;
+    new_modes.c_cc[VMIN]   = 1;
+    new_modes.c_cc[VTIME]  = 0;
+    set_ttymodes(&new_modes);
+    set_tty_crmod(FALSE);
+# else /* USE_SGTTY */
+#   if USE_FCNTL
+      new_modes.sg_flags &= ~CBREAK;
+#   endif
+    new_modes.sg_flags |= RAW;
+    set_ttymodes(&new_modes);
+    {
+      struct tchars tmp_tchars;
+      struct ltchars tmp_ltchars;
+      memset(&tmp_tchars,  -1, sizeof(tmp_tchars));
+      memset(&tmp_ltchars, -1, sizeof(tmp_ltchars));
+      ioctl(0, TIOCSETC, &tmp_tchars);
+      ioctl(0, TIOCSLTC, &tmp_ltchars);
+    }
+# endif
+#endif
+#ifdef SARG10
+    ttybin(1);
+#endif
+#ifdef SARG20
+    ttybin(1);
+    page(0);		/* Turn off all character processing at input */
+    superbin(1);	/* Turn off ^C (among others). Keep your fingers crossed!! */
+#endif
+  } else {
+#ifdef UNIX
+# if USE_POSIX_TERMIOS || USE_TERMIO
+    new_modes = old_modes;	/* FIXME */
+# else /* USE_SGTTY */
+    new_modes.sg_flags &= ~RAW;
+#   if USE_FCNTL
+      new_modes.sg_flags |= CBREAK;
+#   endif
+    ioctl(0, TIOCSETC, &old_tchars);
+    ioctl(0, TIOCSLTC, &old_ltchars);
+# endif
+    set_ttymodes(&new_modes);
+#endif
+#ifdef SARG10
+    ttybin(0);
+#endif
+#ifdef SARG20
+    ttybin(0);
+    superbin(0);	/* Puuuh! We made it!? */
+    page(1);		/* Back to normal input processing */
+    ttybin(1);		/* This must be the mode for DEC20 */
+#endif
+  }
+  dump_ttymodes("...set_tty_raw", enabled);
+}
