c339807dc57a14aca01705d428b58e559fc1a792
Thomas E. Dickey <dickey@invisible-island.net>
2019-01-05 18:43:43 +0000

    snapshot of project "vttest", label t20190105

--- patchlev.h
+++ patchlev.h
@@ -1,4 +1,4 @@
-/* $Id: patchlev.h,v 1.66 2018/11/16 22:18:27 tom Exp $ */
+/* $Id: patchlev.h,v 1.68 2019/01/05 14:41:23 tom Exp $ */
 #define RELEASE 2
 #define PATCHLEVEL 7
-#define PATCH_DATE 20181116
+#define PATCH_DATE 20190105
